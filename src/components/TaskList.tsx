68a2ee7f80d98e335e418d120551903cb1a784ed
Philip Oka <philip.oka@gmail.com>
2021-09-15 20:44:18 -0300

    criando as funções para criar novo todo, completar um todo e apagar um todo. Usando os conceitos de state e imutabilidade

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,37 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
+    const newTask: Task = {
+      id: Math.floor(Math.random() * 100),
+      title: newTaskTitle,
+      isComplete: false
+    }
+    if(newTask.title !== ''){
+      setTasks([...tasks, newTask])
+      setNewTaskTitle('')
+    }
     // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
   }
 
   function handleToggleTaskCompletion(id: number) {
+    const newTasks:Task[] = taskArrayCompletingElement(id);
+    setTasks([...newTasks])
     // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
   }
 
   function handleRemoveTask(id: number) {
     // Remova uma task da listagem pelo ID
+    const newArrayTasks:Task[] = taskArrayExcludingElement(id);
+    setTasks([...newArrayTasks])
+
+  }
+
+  const taskArrayCompletingElement = (id: number) => {
+    return tasks.map((task) => task.id === id ? {...task, isComplete: !task.isComplete} : task)
+  }
+
+  const taskArrayExcludingElement = (id: number) => {
+    return tasks.filter((task) => task.id !== id)
   }
 
   return (
@@ -32,9 +54,9 @@ export function TaskList() {
         <h2>Minhas tasks</h2>
 
         <div className="input-group">
-          <input 
-            type="text" 
-            placeholder="Adicionar novo todo" 
+          <input
+            type="text"
+            placeholder="Adicionar novo todo"
             onChange={(e) => setNewTaskTitle(e.target.value)}
             value={newTaskTitle}
           />
@@ -50,7 +72,7 @@ export function TaskList() {
             <li key={task.id}>
               <div className={task.isComplete ? 'completed' : ''} data-testid="task" >
                 <label className="checkbox-container">
-                  <input 
+                  <input
                     type="checkbox"
                     readOnly
                     checked={task.isComplete}
@@ -66,7 +88,7 @@ export function TaskList() {
               </button>
             </li>
           ))}
-          
+
         </ul>
       </main>
     </section>
